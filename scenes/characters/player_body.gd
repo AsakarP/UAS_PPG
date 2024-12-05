5afc7bbd19b53c1576ab6f5d09c697956f7abc7b
SamSP <113569096+AsakarP@users.noreply.github.com>
2024-12-05 22:28:57 +0700

    Menambah Player Character dan Tilemap

---
+++ scenes/characters/player_body.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@onready var _animated_sprite = $PlayerSprite
+const SPEED = 100
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	# Movement WASD
+	if Input.is_key_pressed(KEY_W):
+		velocity.y -= SPEED
+	if Input.is_key_pressed(KEY_A):
+		$PlayerSprite.flip_h = true
+		velocity.x -= SPEED
+	if Input.is_key_pressed(KEY_S):
+		velocity.y += SPEED
+	if Input.is_key_pressed(KEY_D):
+		velocity.x += SPEED
+		$PlayerSprite.flip_h = false
+	
+	position += velocity * delta
+	
+	if velocity != Vector2.ZERO:
+		_animated_sprite.play("run")
+	else:
+		_animated_sprite.play("idle")
