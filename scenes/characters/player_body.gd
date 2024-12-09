908bca34a0e1e456ddb60633c560958044832ac5
SamSP <2272030@maranatha.ac.id>
2024-12-09 13:15:54 +0700

    Character look

--- scenes/characters/player_body.gd
+++ scenes/characters/player_body.gd
@@ -1,29 +1,33 @@
 extends CharacterBody2D
 
-@onready var _animated_sprite = $PlayerSprite
+@onready var animated_sprite = $PlayerSprite
 const SPEED = 100
 
 func _physics_process(delta):
+	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
+	if mouse_direction.x > 0 and animated_sprite.flip_h:
+		animated_sprite.flip_h = false
+	elif mouse_direction.x < 0 and not animated_sprite.flip_h:
+		animated_sprite.flip_h = true
+		
 	var velocity = Vector2.ZERO
 	# Movement WASD
 	if Input.is_key_pressed(KEY_W):
 		velocity.y -= SPEED
 	if Input.is_key_pressed(KEY_A):
-		$PlayerSprite.flip_h = true
 		velocity.x -= SPEED
 	if Input.is_key_pressed(KEY_S):
 		velocity.y += SPEED
 	if Input.is_key_pressed(KEY_D):
 		velocity.x += SPEED
-		$PlayerSprite.flip_h = false
 	
 	position += velocity * delta
 	
 	# Animasi gerakan
 	if velocity != Vector2.ZERO:
-		_animated_sprite.play("run")
+		animated_sprite.play("run")
 	else:
-		_animated_sprite.play("idle")
+		animated_sprite.play("idle")
 	
 	# Dapat menabrak collision
 	move_and_slide()
