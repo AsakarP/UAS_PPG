950b9469f3aa567201524ed0c91bc5b7514f7e26
Jengtg <113569069+Jengtg@users.noreply.github.com>
2024-12-17 13:37:29 +0700

    Add files via upload

--- scenes/characters/skeleton_body.gd
+++ scenes/characters/skeleton_body.gd
@@ -2,11 +2,19 @@ extends CharacterBody2D
 
 var SPEED = 50
 @export var player: Node2D
+@onready var animated_sprite = $SkeletonSprite
 @onready var SkeleNav = $SkeletonNavigation
 
 func _physics_process(delta):
 	var dir = to_local(SkeleNav.get_next_path_position()).normalized()
 	velocity = dir * SPEED
+	
+	if dir.x > 0 and animated_sprite.flip_h:
+		animated_sprite.flip_h = false
+	elif dir.x < 0 and not animated_sprite.flip_h:
+		animated_sprite.flip_h = true
+		
+	print (velocity)
 	move_and_slide()
 	
 func makePath():
@@ -14,4 +22,5 @@ func makePath():
 
 func _on_timer_timeout():
 	makePath()
+	
 	pass
